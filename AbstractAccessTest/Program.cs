e076c9d357cba50365fec8a86b73811f949a9fdd
Matt Keefer <136768491+Keefer184@users.noreply.github.com>
2024-03-22 17:00:06 -0400

    Update Program.cs
    took out my file paths

--- AbstractAccessTest/Program.cs
+++ AbstractAccessTest/Program.cs
@@ -1,6 +1,6 @@
 ﻿using AbstractAccessTest;
 
-ParqDir parqDir = new ParqDir(@"C:\Users\keefe\Documents");
+ParqDir parqDir = new ParqDir(@"[YOUR FILE DIRECTORY]");
 DateTime start = new DateTime(2023, 1, 25);
 DateTime end = new DateTime(2023, 2,1);
 DateTime[] dateRange = new DateTime[] { start, end };
